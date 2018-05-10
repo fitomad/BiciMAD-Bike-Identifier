f05240bfb3ac5a68cdddbcc083d7797c2d9ba6cb
Adolfo <fitomad@users.noreply.github.com>
2018-05-10 23:31:50 +0200

    Update UIColor+HexCode.swift
    Comentario de cabecera de archivo

--- Swift Code/BiciMad Incidencias/Extensions/UIColor+HexCode.swift
+++ Swift Code/BiciMad Incidencias/Extensions/UIColor+HexCode.swift
@@ -1,6 +1,10 @@
-/*
-
-*/
+//
+//  UIColor+HexCode
+//  desappstre framework
+//
+//  Created by Adolfo on 19/05/15.
+//  Copyright (c) 2015 Desappstre Studio. All rights reserved.
+//
 
 import UIKit
 
